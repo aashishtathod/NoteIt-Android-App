01f59b79cd84754dce21a114baf0eb4f6430ee69
Aashishkumar Tathod <aashishtathod@outlook.com>
2023-02-05 12:22:06 +0530

    [IMP] Added NoteItApplication class to configure hilt

--- app/src/main/java/dev/aashishtathod/noteit/MainActivity.kt
+++ app/src/main/java/dev/aashishtathod/noteit/MainActivity.kt
@@ -10,34 +10,37 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import dev.aashishtathod.noteit.ui.navigation.NoteItNavigation
 import dev.aashishtathod.noteit.ui.theme.NoteItTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContent {
-			NoteItTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colors.background
-				) {
-					Greeting("Android")
-				}
-			}
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteItTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    NoteItNavigation()
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	NoteItTheme {
-		Greeting("Android")
-	}
+    NoteItTheme {
+        Greeting("Android")
+    }
 }
